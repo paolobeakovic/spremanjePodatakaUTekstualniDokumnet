a0daece99ddaf37d95f1a3d11eadafc9dce55686
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-06-18 10:18:52 +0200

    Add files via upload

---
+++ spremanjePodatakaUTekstualniDokument/spremanjePodatakaUTekstualniDokument/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spremanjePodatakaUTekstualniDokument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folder = @"C:\Vjezba";
+            string fileName = "Test.txt";
+            string fullPath = folder + fileName;
+            string[] test = { Console.ReadLine() };
+            File.WriteAllLines(fullPath, test);
+            string readText = File.ReadAllText(fullPath);
+            Console.WriteLine(readText);
+            Console.ReadKey();
+        }
+    }
+}
